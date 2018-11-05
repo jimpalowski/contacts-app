0f5d278d075d85247cb74665e1353f8b604ecff9
Jim Palowski <palowskijim@gmail.com>
2018-11-04 23:39:19 -0800

    Add BrowserRouter

--- src/index.js
+++ src/index.js
@@ -11,3 +11,18 @@ ReactDOM.render(
     </BrowserRouter>
         , document.getElementById('root'));
 registerServiceWorker();
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+
+ReactDOM.render(
+    <BrowserRouter> 
+        <App />
+    </BrowserRouter>, 
+    document.getElementById('root'));
+    
+registerServiceWorker();
+
