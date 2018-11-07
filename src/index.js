0e9254a04756802aa869d8ef3b1ad0580b012add
Jim Palowski <palowskijim@gmail.com>
2018-11-06 23:53:13 -0800

    Remove extra lines

--- src/index.js
+++ src/index.js
@@ -1,18 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
-import { BrowserRouter } from 'react-router-dom';
-
-ReactDOM.render(
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>
-        , document.getElementById('root'));
-registerServiceWorker();
-import React from 'react';
-import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
